1b5d28c12690197bb6a5eb272b62aad6ab1fde00
Luckyboy Maliehe <luckyboymaliehe@gmail.com>
2025-11-24 23:11:36 +0200

    UseEffect hooks functionality

--- src/components/form-card/form-card.component.jsx
+++ src/components/form-card/form-card.component.jsx
@@ -4,16 +4,39 @@ import './form-card.styles.css'
 
 const FormCard = () => {
 
+
     const[description, setDescription] = useState('');
+    const [title, setTitle] = useState('');
+    const [idea, setIdea] = useState({});
     const redZone = 100;
 
-    const handleChange = (event) => {
-
+    const descriptionChange = (event) => {
          setDescription(event.target.value);
     }
 
+    const titleChange = (event) => {
+        setTitle(event.target.value);
+        }
+
     const counterColorChange = {
         color: description.length >= redZone ? 'red' : 'black',
+    }
+
+    const addIdea = () => {
+        if(description.length != 0 || title.length != 0){
+            const newIdea = {
+                title: title,
+                description: description
+            };
+            setIdea(newIdea)
+            console.log(JSON.stringify(newIdea));
+        }
+        
+        
+    }
+
+    const checkingValues = () => {
+        console.log(idea);
     }
 
 
@@ -22,20 +45,25 @@ const FormCard = () => {
             <h2>Create New Card</h2>
             <div className="title-container">
                 <label>Title:</label>
-                <input placeholder="Enter your idea title"/>
+                <input value={title} onChange={titleChange} placeholder="Enter your idea title"/>
             </div>
              <div className="desc-container">
                 <label>Description:</label>
                 <textarea 
                 className="description-input"
                 value={description} 
-                onChange={handleChange}
+                onChange={descriptionChange}
                 maxLength={140}
-                rows={10}
+                rows={8}
                 cols={50}
                 placeholder="Enter your idea descrption"/>
             </div>
+            <div className="btnContainer">
+                <button onClick={addIdea}>Save</button>
+                <button onClick={checkingValues}>Check</button>
+            </div>
             <p style={counterColorChange} className="">{description.length}/140</p>
+            
             
         </div>
     );
