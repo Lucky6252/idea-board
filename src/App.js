23883940aa609de638c249e14ad204e08c89bd3f
Luckyboy Maliehe <luckyboymaliehe@gmail.com>
2025-11-25 20:50:20 +0200

    close to the solution

--- src/App.js
+++ src/App.js
@@ -2,29 +2,44 @@ import Head from "./components/head/head.component";
 import CardList from "./components/card-list/card-list.component";
 import "./App.css";
 import { useEffect, useState } from "react";
-import { DataFromLocalStorage } from "./utils/hooks.component";
-
 
 function App() {
+  const [ideasList, setIdeasList] = useState([]);
 
-  const [ideas, setIdeas] = useState(() => {
-    const localStorageData = localStorage.getItem('ideas')
-    if(localStorageData){
-      return JSON.parse(localStorageData);
+  const [ideas, setIdeas] = useState([]);
+
+  setIdeas([
+    {
+      title: "Testing title",
+      description: "Test Description",
+    },
+    {
+      title: "Testing title 2",
+      description: "Test Description 2",
+    },
+    {
+      title: "Testing title 3",
+      description: "Test Description 3",
+    },
+    {
+      title: "Testing title 4",
+      description: "Test Description 4",
+    },
+  ]);
+
+  useEffect(() => {
+    const data = localStorage.getItem("ideas");
+    if (data) {
+      setIdeas(JSON.parse(data));
     }
-  });
+  }, []);
 
-  // useEffect(()=>{
-  //   setIdeas(DataFromLocalStorage)
-  // }, []);
-
-console.log(ideas);
-
+  console.log(ideas);
 
   return (
     <div className="App">
-      <Head/>
-      <CardList ideaCards={ideas}/>
+      <Head />
+      {/* <CardList ideas={ideas} /> */}
     </div>
   );
 }
